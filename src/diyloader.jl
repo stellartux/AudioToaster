878c190bd3fcc6e43dd29dc60bd06db9925c230c
Danny Robinson <dannyrobinson@gmx.com>
2018-12-11 23:12:44 +0000

    Initial commit

---
+++ src/diyloader.jl
@@ -0,0 +1,246 @@
+module DIYLoader
+
+#=
+boards.VeroBoard
+boards.TriPadBoard
+boards.Breadboard
+connectivity.TraceCut
+connectivity.CopperTrace
+connectivity.GroundFill
+
+passive.PotentiometerPanel
+passive.TrimmerPotentiometer
+passive.PotentiometerSymbol
+
+passive.InductorSymbol
+
+semiconductors.BJTSymbol
+
+semiconductors.TransistorTO92
+semiconductors.TransistorTO1
+semiconductors.TransistorTO220
+semiconductors.TransistorTO3
+passive.ElectrolyticCanCapacitor
+semiconductors.DIL__IC
+
+semiconductors.ICSymbol
+
+=#
+
+export process, loadfile
+
+using LightXML
+using ACME
+using WAV
+
+function process(model::DiscreteModel, inputfile::String, outputfile::String)
+    y, fs, nbits = wavread(inputfile)
+    processed = run!(model, reshape(y, 1, :))
+    wavwrite(reshape(processed, length(processed), 1), outputfile, Fs=fs, nbits=nbits)
+end
+
+struct Point
+    x::Real
+    y::Real
+end
+Point(el::XMLElement) = Point(parse(Float64, attribute(el, "x")), parse(Float64, attribute(el, "y")))
+Point(xy::Union{Array,Tuple}) = Point(xy[1], xy[2])
+
+struct VeroBoard
+    topleft::Point
+    bottomright::Point
+    horizontal::Bool
+end
+VeroBoard(c::XMLElement) = VeroBoard(Point(find_element(c, "firstPoint")),
+              Point(find_element(c, "secondPoint")),
+              content(find_element(c, "orientation")) == "HORIZONTAL")
+
+struct TraceCut
+    location::Point
+    betweentraces::Bool
+end
+
+struct Wire
+    startpoint::Point
+    endpoint::Point
+end
+Wire(ps::Union{Array{Point},Tuple{Point,Point}}) = Wire(ps[1], ps[2])
+
+function iswithin(board::VeroBoard, point::Point)::Bool
+    return board.topleft.x < point.x &&
+        board.bottomright.x > point.x &&
+        board.topleft.y < point.y &&
+        board.bottomright.y > point.y
+end
+
+function findcomponentbyname(haystack::XMLElement, needle::Union{Regex,String})
+    for component in child_elements(haystack)
+        if occursin(needle, content(find_element(component, "name")))
+            return component
+        end
+    end
+end
+
+function addconnection!(circ, col, p::Point, pin)
+    haskey(col, p) ? connect!(circ, pin, col[p]) : col[p] = pin
+end
+
+function addtwoleggedcomponent!(circ, col, comp, ps::Union{Array{Point},Tuple{Point,Point}})
+    addconnection!(circ, col, ps[1], (comp, (1, 1)))
+    addconnection!(circ, col, ps[2], (comp, (1, -1)))
+end
+
+"""
+Given an element loaded from a diy file, returns the value of that element
+i.e. Returns the resistance of resistors, the capacitance of capacitors, etc
+"""
+function getvalue(el::XMLElement)::Float64
+    value = parse(Float64, content(find_element(find_element(el, "value"), "value")))
+    unit = content(find_element(find_element(el, "value"), "unit"))[1]
+    return convertunitmagnitude(value, unit)
+end
+
+function convertunitmagnitude(value::Real, unit::Char)::Real
+    unitdict = Dict{Char, Float64}(
+        'K'=>1e3,
+        'k'=>1e3,
+        'M'=>1e6,
+        'p'=>1e-12,
+        'n'=>1e-9,
+        'u'=>1e-6,
+        'm'=>1e-3
+    )
+    if haskey(unitdict, unit)
+        value *= unitdict[unit]
+    end
+    return value
+end
+
+function convertunitmagnitude(value::Real, unit::String)::Real
+    return length(unit) == 0 ? value : convertunitmagnitude(value, unit[1])
+end
+
+"""
+Loads a circuit from a .diy file
+"""
+function loadfile(filepath::String)
+  circ = Circuit()
+  boards = []
+  wires = []
+  tracecuts = []
+
+  # references to all of the connected pins
+  connections = Dict{Point, Union{Tuple,Symbol}}()
+
+  file = parse_file(filepath)
+  fr = root(file)
+  gridspacing = parse(Float64,
+      content(find_element(find_element(fr, "gridSpacing"), "value")))
+
+  cs = find_element(fr, "components")
+
+  for c in child_elements(cs)
+      n = SubString(name(c), 22)
+
+      # store boards until all components are collected
+      if n == "boards.VeroBoard"
+          push!(boards, VeroBoard(c))
+
+      elseif startswith("passive.Resistor", n)
+          addtwoleggedcomponent!(circ, connections,
+              add!(circ, resistor(getvalue(c)))
+              map(Point, child_elements(find_element(c, "points"))))
+
+      elseif n == "passive.RadialCeramicDiskCapacitor" ||
+          n == "passive.AxialElectrolyticCapacitor" ||
+          n == "passive.RadialElectrolytic" ||
+          n == "passive.AxialFilmCapacitor" ||
+          n == "passive.RadialFilmCapacitor" ||
+          n == "passive.CapacitorSymbol"
+
+          addtwoleggedcomponent!(circ, connections,
+            add!(circ, capacitor(getvalue(c))),
+            map(Point, child_elements(find_element(c, "points"))))
+
+      elseif n == "connectivity.HookupWire"
+          pointelems = collect(child_elements(find_element(c, "controlPoints")))
+          points = (Point(pointelems[1]), Point(pointelems[4]))
+          if occursin(r"g(rou)?nd"i, content(find_element(c, "name")))
+              for p in points
+                  addconnection!(circ, connections, p, :gnd)
+              end
+          else
+              push!(wires, Wire(points))
+          end
+
+      elseif n == "connectivity.Jumper" || n == "connectivity.Line"
+          push!(wires, Wire(map(Point, child_elements(find_element(c, "points")))))
+
+      elseif n == "electromechanical.OpenJack1__4" ||
+        n == "electromechanical.CliffJack1__4" ||
+        n == "electromechanical.ClosedJack1__4"
+
+          jackname = content(find_element(c, "name"))
+          ps = map(Point, collect(child_elements(find_element(c, "controlPoints"))))
+
+          if occursin(r"output"i, jackname)
+              addtwoleggedcomponent!(circ, connections, add!(circ, voltageprobe()), ps)
+          elseif occursin(r"input"i, jackname)
+              addtwoleggedcomponent!(circ, connections, add!(circ, voltagesource()), ps)
+          end
+
+      elseif n == "electromechanical.PlasticDCJack"
+          dcmatch = match(r"(-?[\d\.]+)([A-Za-z]*)", content(find_element(c, "value")))
+          dcval = convertunitmagnitude(parse(Float64, dcmatch[1]), dcmatch[2])
+          points = map(Point, collect(child_elements(find_element(c, "controlPoints"))))
+          ps = (points[1], points[3])
+          addtwoleggedcomponent!(circ, connections, add!(circ, voltagesource(dcval)), ps)
+
+      elseif n == "misc.BatterySymbol"
+          value = parse(Float64, content(find_element(find_element(el, "voltageNew"), "value")))
+          unit = content(find_element(find_element(el, "voltageNew"), "unit"))[1]
+          addtwoleggedcomponent(circ, connections,
+            add!(circ, voltagesource(convertunitmagnitude(value, unit))),
+            map(Point, child_elements(find_element(c, "points")))))
+
+      elseif n == "misc.GroundSymbol"
+          addconnection!(circ, connections,
+              Point(find_element(c, "point")),
+              :gnd)
+
+      elseif n == "semiconductors.DiodeSymbol" ||
+          n == "semiconductors.SchottkyDiodeSymbol" ||
+          n == "semiconductors.ZenerDiodeSymbol" ||
+          n == "semiconductors.DiodeGlass" ||
+          n == "semiconductors.DiodePlastic" ||
+          n == "semiconductors.LEDSymbol" ||
+          n == "semiconductors.LED"
+
+          # TODO: load diode specifics from diode name
+          # for now, default diode will do
+
+          addtwoleggedcomponent!(circ, connections,
+            add!(circ, diode()),
+            map(Point, child_elements(find_element(c, "points"))))
+
+      end
+  end
+
+  # connect the wires
+  for w in wires
+      if haskey(connections, w.startpoint) && haskey(connections, w.endpoint)
+          connect!(circ, connections[w.startpoint], connections[w.endpoint])
+      elseif haskey(connections, w.startpoint) && !haskey(connections, w.endpoint)
+          addconnection!(circ, connections, w.endpoint, connections[w.startpoint])
+      elseif haskey(connections, w.endpoint) && !haskey(connections, w.startpoint)
+          addconnection!(circ, connections, w.startpoint, connections[w.endpoint])
+      else
+          # handle wires connected to boards or other wires here
+      end
+  end
+
+  free(file)
+  return circ
+end
+
+end # module DIYLoader
---
+++ test/runtests.jl
@@ -0,0 +1,21 @@
+using ACME
+using Test
+
+include("..\\src\\diyloader.jl")
+
+circuitfrommacro = @circuit begin
+    r1 = resistor(1e4)
+    c1 = capacitor(4.7e-9), [1] ⟷ r1[2], [2] ⟷ gnd
+    src = voltagesource(), [+] ⟷ r1[1], [-] ⟷ gnd
+    prb = voltageprobe(), [+] ⟷ c1[1], [-] ⟷ gnd
+end
+circuitfromfile = DIYLoader.loadfile("acme\\examples\\simplest.diy")
+modelfrommacro = DiscreteModel(circuitfrommacro, 1/44100)
+modelfromfile = DiscreteModel(circuitfromfile, 1/44100)
+testdata = reshape(rand(1000) .* 2.0 .- 1.0, 1, :)
+expected = run!(modelfrommacro, testdata)
+actual = run!(modelfromfile, testdata)
+
+for i in 1:length(expected)
+    @test expected[i] ≈ actual[i]
+end
